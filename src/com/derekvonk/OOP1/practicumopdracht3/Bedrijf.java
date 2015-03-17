b9f6e4bdabadc299ac162ce123062c3864facf1a
Derek Vonk <derekvonk@me.com>
2015-03-17 16:20:22 +0100

    new commit

--- src/com/derekvonk/OOP1/practicumopdracht3/Bedrijf.java
+++ src/com/derekvonk/OOP1/practicumopdracht3/Bedrijf.java
@@ -1,14 +1,63 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Derek Vonk - 500704534 - IDI101 Semester 2 - Propedeuse 
  */
 package com.derekvonk.OOP1.practicumopdracht3;
 
+import java.util.ArrayList;
 /**
+ * Bedrijf Class for Bedrijf en Werknemer Practicum Opdracht 3
  *
- * @author Vonk
+ * @version 0.1 - March 2015
+ * @author Derek Vonk - 500704534 - IDI101 - Practicum Opdracht 3 -
+ * Object Oriented Programming 1 - Docent Michel Mercera
  */
 public class Bedrijf {
     
+    private ArrayList<String> werknemers = new ArrayList<>();
+    private String naam = "";
+    
+    /**
+     * Default constructor for Bedrijf Object
+     */
+    public Bedrijf() {
+        
+    }
+    
+    /**
+     * Constructor creates Object of Bedrijf with name as the parameter
+     * @param naam String
+     */
+    public Bedrijf(String naam) {
+        this.naam = naam;
+    } 
+  
+    /**
+     * Method
+     */
+    public void betaalSalarissen() {
+        
+    }
+    
+    /**
+     * Method returns the number of managers from ?
+     * @return Integer
+     */
+    public int aantalManagers() {
+        return 0;
+    }
+    
+    /**
+     * Method passes an object in to ...
+     */
+    public void neemInDienst(Persoon persoon) {
+        
+    }
+    
+    /**
+     * Method returns a String representation of input
+     * @return String
+     */
+    public String toString() {
+        return "";
+    }
 }
