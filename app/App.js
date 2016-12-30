36a636582d238c228d41281141fa375491d155b9
Derek Vonk <derek.vonk@hva.nl>
2016-12-30 20:05:38 +0100

    basic empty starter code to App.js

--- app/App.js
+++ app/App.js
@@ -1,5 +1,30 @@
-import React, { Component } form 'react';
+import React, { Component } from 'react';
 
 import {
-    
+    View,
+    ScrollView,
+    StyleSheet
+} from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return(
+            <View style={ styles.container }>
+                <ScrollView style={ styles.content }>
+                    <View />
+                </ScrollView>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5'
+    },
+    content: {
+        flex: 1,
+        paddingTop: 60
+    }
+})
