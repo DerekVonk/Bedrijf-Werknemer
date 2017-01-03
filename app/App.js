5fa2dfda12bb272b01f733c50fe365a603cad625
Derek Vonk <derek.vonk@hva.nl>
2017-01-03 15:07:43 +0100

    add inputValue and inputChange function to store input from user in state

--- app/App.js
+++ app/App.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 
 import Heading from './Heading';
+import Input from './Input';
 
 export default class App extends Component {
     constructor() {
@@ -18,11 +19,25 @@ export default class App extends Component {
             type: 'All'
         }
     }
+    // Here the inputChange method is created, which takes inputValue as an argument.
+    // this takes changes from the Input value and stores it in the state
+    inputChange(inputValue) {
+        // Log out the inputValuevalue to make sure the method is working
+        console.log('Input value: ', inputValue);
+        // set the state with the new value (same as this.setState({ inputValue: inputValue }) )
+        this.setState({ inputValue });
+    }
+
+
     render() {
+        const { inputValue } = this.state
         return(
             <View style={ styles.container }>
                 <ScrollView style={ styles.content }>
                     <Heading />
+                    <Input
+                    inputValue={ inputValue }
+                    inputChange={ (text) => this.inputChange(text) } />
                 </ScrollView>
             </View>
         )
--- app/Input.js
+++ app/Input.js
@@ -7,14 +7,23 @@ import {
 } from 'react-native';
 
 export default class Input extends Component {
+    constructor(props){
+        super(props);
+        console.log('these are the props in Input.js: ', props);
+    }
+
     render() {
+        // destructuring props
+        const {inputValue, inputChange} = this.props;
         return(
-            <View style={ styles.inputContainer }>
+            <View style={styles.inputContainer}>
                 <TextInput
-                style={ styles.input }
-                placeholder="What needs to be done?"
-                placeholderTextColor='#CACACA'
-                selectionColor='#666666'/>
+                    value={inputValue}
+                    style={styles.input}
+                    placeholder="What needs to be done?"
+                    placeholderTextColor='#CACACA'
+                    selectionColor='#666666'
+                    onChangeText={inputChange}/>
             </View>
         )
     }
